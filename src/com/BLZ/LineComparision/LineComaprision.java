0ed6c4044d178ac4b7e7919f02e3a553ba1d9fb3
NiteshSatam <niteshsatam55@gmail.com>
2022-07-10 19:50:36 +0530

    Added: Readme and gitignore file

---
+++ src/com/BLZ/LineComparision/LineComaprision.java
@@ -0,0 +1,10 @@
+package com.BLZ.LineComparision;
+
+public class LineComaprision {
+
+	public static void main(String[] args) {
+
+		System.out.println("Welcome to Line Comparison Computation Program");
+	}
+
+}
