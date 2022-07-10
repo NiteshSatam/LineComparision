5e48df8b64cf33c166978dc1b33b95a678d39b9b
NiteshSatam <niteshsatam55@gmail.com>
2022-07-10 20:21:36 +0530

    Added: Check Equality Logic

--- src/com/BLZ/LineComparision/LineComaprision.java
+++ src/com/BLZ/LineComparision/LineComaprision.java
@@ -6,9 +6,19 @@ public class LineComaprision {
 
 		int x1 = 50, y1 = 100;
 		int x2 = 200, y2 = 400;
-		System.out.println("Welcome to Line Comparison Computation Program");
+		int x3 = 50, y3 = 100;
+		int x4 = 200, y4 = 400;
+		System.out.println("Welcome to Line Comparision Computation Program");
 		double length1 = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
 		System.out.println("Length of line1: " + length1);
+		double length2 = Math.sqrt(Math.pow(x3 - x4, 2) + Math.pow(y3 - y4, 2));
+		System.out.println("Length of line2: " + length2);
+		
+		if (length1 == length2)
+			System.out.println("Both lines are equal");
+		
+		else
+			System.out.println("Both lines are not equal");
 	}
 
 }
